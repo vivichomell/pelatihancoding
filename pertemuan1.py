8d5c6d780a5e84eae5faa3dd8aeab4ead3a21092
vivichomell <143859503+vivichomell@users.noreply.github.com>
2023-09-02 10:42:54 +0700

    first commit

---
+++ pertemuan1.py
@@ -0,0 +1,38 @@
+# # a = 52
+# # print(a)
+# # print(type(a))
+# # a2 = 52
+# # print(type(a2))
+# # nama_apel = "Apel Malang"
+# # print (nama_apel)
+# # print (type(nama_apel))
+# # bool
+# # apel_benar = True
+# # apel_salah = False
+# # print (type(apel_benar))
+# # print (apel_salah)
+# # list
+# list_warna_apel = ['hijau','merah','kuning']
+# list_apel = ["manis",2,3.0]
+# print(list_warna_apel)
+# print(list_apel)
+# # tuple
+# tuple_jumlah = (2,5.25, "satu")
+# print(tuple_jumlah)
+# #dictinary
+# apel_dictionary = {'buah':'apel','warna':'hijau','jumlah': '5'}
+# print(apel_dictionary['buah'])
+
+# print(34+2)
+# print(30-4)
+# print(2*6)
+# print(9/3)
+
+#penjumlahan 
+b = 39
+e = 4
+print(b + e)
+#pengurangan
+b = 39
+e = 4
+print(b - e)
